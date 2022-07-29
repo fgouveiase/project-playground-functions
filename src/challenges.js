6073832ed65f70c0a34afb827081c316bd8ce868
Felipe Gouveia <felipegouveia.bfr@hotmail.com>
2022-07-29 18:04:55 -0300

    Resolvendo questao 2

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,9 @@ function compareTrue(girafa, elefante, macaco) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
