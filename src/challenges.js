2157829c659db57de84d0af40be35dcc928793e8
Felipe Gouveia <felipegouveia.bfr@hotmail.com>
2022-08-01 18:37:59 -0300

    corrigindo segunda feira questao 8

--- src/challenges.js
+++ src/challenges.js
@@ -68,17 +68,17 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numbers) {
   let array = [];
-  for (let i = 0; i < array.length; i+=) {
-  if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
-    array.push('fizzBuzz');
-    } else if (numbers[i] % 3 === 0 && numbers[i] % 5 !== 0) {
+  for (let i = 0; i < array.length; i++) {
+    if ( numbers[i] % 3 === 0 && numbers[i] % 5 !== 0) {
       array.push('fizz');
     } else if (numbers[i] % 3 !== 0 && numbers[i] % 5 === 0) {
       array.push('buzz');
+    } else if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+      array.push('fizzBuzz');
     } else {
       array.push('bug!');
     }
-  }
+  } 
   return array;
 }
 
