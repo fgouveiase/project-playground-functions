e2e2676870defa0b8b15a8421413c04f9a06ea10
Felipe Gouveia <felipegouveia.bfr@hotmail.com>
2022-08-01 20:57:04 -0300

    questao 8 alterando length

--- src/challenges.js
+++ src/challenges.js
@@ -68,12 +68,12 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numbers) {
   let array = [];
-  for (let i = 0; i < array[i].length; i++) {
-    if ((numbers[i] % 3 === 0) && (numbers[i] % 5 === 0)) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
       array.push('fizzBuzz');
-    } else if ((numbers[i] % 3 === 0) && (numbers[i] % 5 !== 0)) {
+    } else if (numbers[i] % 3 === 0 && numbers[i] % 5 !== 0) {
       array.push('fizz');
-    } else if ((numbers[i] % 5 === 0) && (numbers[i] % 3 !== 0)) {
+    } else if (numbers[i] % 5 === 0 && numbers[i] % 3 !== 0) {
       array.push('buzz');
     } else {
       array.push('bug!');
